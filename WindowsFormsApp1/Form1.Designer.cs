637f124d669f86f2665ab0ad285c9ee12e4bca7b
Luna <luna.sielawa@gmail.com>
2023-11-09 12:39:27 +0100

    up2

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            bubble = new Button();
+            selection = new Button();
+            insert = new Button();
+            merge = new Button();
+            quick = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(193, 16);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 0;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(214, 12);
+            numericUpDown1.Maximum = new decimal(new int[] { 2000000, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // bubble
+            // 
+            bubble.Location = new Point(48, 146);
+            bubble.Name = "bubble";
+            bubble.Size = new Size(40, 23);
+            bubble.TabIndex = 4;
+            bubble.Text = "sb";
+            bubble.UseVisualStyleBackColor = true;
+            bubble.Click += sb_Click;
+            // 
+            // selection
+            // 
+            selection.Location = new Point(94, 146);
+            selection.Name = "selection";
+            selection.Size = new Size(40, 23);
+            selection.TabIndex = 5;
+            selection.Text = "ss";
+            selection.UseVisualStyleBackColor = true;
+            selection.Click += selection_Click;
+            // 
+            // insert
+            // 
+            insert.Location = new Point(140, 146);
+            insert.Name = "insert";
+            insert.Size = new Size(40, 23);
+            insert.TabIndex = 6;
+            insert.Text = "si";
+            insert.UseVisualStyleBackColor = true;
+            insert.Click += button4_Click;
+            // 
+            // merge
+            // 
+            merge.Location = new Point(186, 146);
+            merge.Name = "merge";
+            merge.Size = new Size(40, 23);
+            merge.TabIndex = 7;
+            merge.Text = "sm";
+            merge.UseVisualStyleBackColor = true;
+            merge.Click += merge_Click;
+            // 
+            // quick
+            // 
+            quick.Location = new Point(232, 146);
+            quick.Name = "quick";
+            quick.Size = new Size(40, 23);
+            quick.TabIndex = 8;
+            quick.Text = "sq";
+            quick.UseVisualStyleBackColor = true;
+            quick.Click += quick_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(143, 111);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Czas: ";
+            label1.Click += label1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(193, 108);
+            textBox2.MaxLength = 5000000;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(135, 23);
+            textBox2.TabIndex = 11;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 108);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonShadow;
+            ClientSize = new Size(340, 181);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(quick);
+            Controls.Add(merge);
+            Controls.Add(insert);
+            Controls.Add(selection);
+            Controls.Add(bubble);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Controls.Add(checkBox1);
+            MaximumSize = new Size(356, 220);
+            MinimumSize = new Size(356, 220);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+        private Button bubble;
+        private Button selection;
+        private Button insert;
+        private Button merge;
+        private Button quick;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private Stopwatch stopwatch = new Stopwatch();
+
+        private double getTime()
+        {
+            if (!stopwatch.IsRunning)
+            {
+                stopwatch.Start();
+            }
+
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] numbers = GetNumbersFromTextBoxOrGenerated();
+            double startTime = getTime();
+            numbers = InsertionSort(numbers);
+            textBox3.Text = string.Join(" ", numbers);
+
+            double endTime = getTime();
+            double elapsedTime = (endTime - startTime) / 1000.0;
+
+            textBox2.Text = $"\n{elapsedTime}";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int[] Convert(string napis)
+        {
+            var liczbyS = napis.Trim().Split(' ');
+            var liczby = new int[liczbyS.Length];
+            //Split ma parametr i ma omijaæ puste wartoœæi
+            for (int i = 0; i < liczbyS.Length; i++)
+            {
+                liczby[i] = int.Parse(liczbyS[i].Trim());
+            }
+            return liczby;
+        }
+
+        private int[] GenerateRandomNumbers(int count)
+        {
+            Random rand = new Random();
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = rand.Next(0, 20);
+            }
+
+            return numbers;
+        }
+
+        private void sb_Click(object sender, EventArgs e)
+        {
+
+            int[] numbers = GetNumbersFromTextBoxOrGenerated();
+
+            double startTime = getTime();
+
+            numbers = BubbleSort(numbers);
+
+            textBox3.Text = string.Join(" ", numbers);
+
+            double endTime = getTime();
+            double elapsedTime = (endTime - startTime) / 1000.0;
+
+            textBox2.Text = $"\n{elapsedTime}";
+
+        }
+
+
+        private int[] BubbleSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+
+        private int[] InsertionSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 1; i < length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            return arr;
+        }
+
+        private int[] SelectionSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                int minIdx = i;
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+                int temp = arr[minIdx];
+                arr[minIdx] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+
+
+        private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int swap = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = swap;
+
+            return i + 1;
+        }
+
+        private int[] GetNumbersFromTextBoxOrGenerated()
+        {
+            int[] numbers;
+
+            if (checkBox1.Checked)
+            {
+                int count = (int)numericUpDown1.Value;
+                numbers = GenerateRandomNumbers(count);
+            }
+            else
+            {
+                numbers = Convert(textBox1.Text);
+            }
+
+            return numbers;
+        }
+
+        private void selection_Click(object sender, EventArgs e)
+        {
+            int[] numbers = GetNumbersFromTextBoxOrGenerated();
+            double startTime = getTime();
+            numbers = SelectionSort(numbers);
+            textBox3.Text = string.Join(" ", numbers);
+
+            double endTime = getTime();
+            double elapsedTime = (endTime - startTime) / 1000.0;
+
+            textBox2.Text = $"\n{elapsedTime}";
+        }
+
+        private void quick_Click(object sender, EventArgs e)
+        {
+            int[] numbers = GetNumbersFromTextBoxOrGenerated();
+            double startTime = getTime();
+            QuickSort(numbers, 0, numbers.Length - 1);
+
+
+            textBox3.Text = string.Join(" ", numbers);
+
+            double endTime = getTime();
+            double elapsedTime = (endTime - startTime) / 1000.0;
+
+            textBox2.Text = $"\n{elapsedTime}";
+        }
+
+
+
+        private void QuickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(arr, low, high);
+                QuickSort(arr, low, pi - 1);
+                QuickSort(arr, pi + 1, high);
+            }
+        }
+
+        private void merge_Click(object sender, EventArgs e)
+        {
+            int[] numbers = GetNumbersFromTextBoxOrGenerated();
+
+            double startTime = getTime();
+
+            numbers = MergeSort(numbers);
+
+            textBox3.Text = string.Join(" ", numbers);
+
+            double endTime = getTime();
+            double elapsedTime = (endTime - startTime) / 1000.0;
+
+            textBox2.Text = $"\n{elapsedTime}";
+        }
+
+
+
+        public static int[] MergeSort(int[] array)
+        {
+            if (array.Length <= 1)
+                return array;
+
+            int middleIndex = array.Length / 2;
+            int[] leftArray = array.Take(middleIndex).ToArray();
+            int[] rightArray = array.Skip(middleIndex).ToArray();
+
+            return Merge(MergeSort(leftArray), MergeSort(rightArray));
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            List<int> result = new List<int>();
+            int leftIndex = 0, rightIndex = 0;
+
+            while (leftIndex < left.Length && rightIndex < right.Length)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    result.Add(left[leftIndex++]);
+                }
+                else
+                {
+                    result.Add(right[rightIndex++]);
+                }
+            }
+
+            while (leftIndex < left.Length)
+            {
+                result.Add(left[leftIndex++]);
+            }
+
+            while (rightIndex < right.Length)
+            {
+                result.Add(right[rightIndex++]);
+            }
+
+            return result.ToArray();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
